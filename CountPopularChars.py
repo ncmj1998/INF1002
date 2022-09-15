2638c17e690ba46c7abff00bb4853be908f928a5
Norman Chia <norman.chia.minjie@gmail.com>
2022-09-15 10:12:38 +0800

    Lab 2

---
+++ CountPopularChars.py
@@ -0,0 +1,28 @@
+import sys
+
+if len(sys.argv) < 2:
+    print("Invalid input")
+    exit()
+
+input = sys.argv[1] #get user input
+input = input.lower()#convert input to lower case
+
+letters_dict = {}
+for letter in input: #if letter in dictionary, add count
+    if letter in letters_dict:
+        letters_dict[letter] = letters_dict[letter] +1
+    else: #else create new pair
+        letters_dict[letter] = 1
+
+#sort dictionary in descending order by value
+letters_dict = dict(sorted(letters_dict.items(), key=lambda item: item[1],reverse=True))
+
+count = 0
+output = ""
+for pair in letters_dict:
+    if count < 5:
+        output += f"{pair}:{letters_dict[pair]}"
+        count +=1
+        output += ","
+
+print(output[:-1]) #exclude last char
---
+++ EvenOddCalculator.py
@@ -0,0 +1,36 @@
+import sys
+
+inputs = []
+for i in range(1,len(sys.argv)):
+    input = sys.argv[i]
+    if input.isdigit():
+        inputs.append(int(sys.argv[i]))
+    else:
+        print("Invalid input")
+        exit()
+inputs.sort() ##sort input in ascending order
+
+even_count = 0
+odd_count = 0
+even_total = 0
+odd_total = 0
+biggest_number = inputs[-1]
+smallest_number = inputs[0]
+total = 0
+
+for input in inputs:
+    total += input
+    if (input%2) == 0:
+        even_count+=1
+        even_total+=input
+    else:
+        odd_count+=1
+        odd_total+=input
+centered_avg = (total - biggest_number - smallest_number) / (len(inputs)-2)
+
+output = f"The sum of all even numbers is {even_total}, the sum of all odd numbers is {odd_total}," \
+         f"the difference between the biggest and smallest number is {biggest_number-smallest_number}, the " \
+         f"total number of even numbers is {even_count}, the total number of odd numbers is " \
+         f"{odd_count}, the centered average is {centered_avg:0.0f}."
+
+print(output)
